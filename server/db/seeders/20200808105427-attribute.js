128f186ea667fec536045c71625d6adceaa9fbff
zubi <azu@gmail.com>
2020-08-10 09:11:26 +0100

    changed somethings in seeder

--- server/db/seeders/20200808105427-attribute.js
+++ server/db/seeders/20200808105427-attribute.js
@@ -1,21 +1,23 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('attribute', [{
-        // INSERT INTO attribute(attribute_id, name) VALUES(1, 'Size'), (2, 'Color')
-        attribute_id: 1,
-        name: 'Size'
-      },
-      {
-        attribute_id: 2,
-        name: 'Color'
-      }
-    ], {});
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('attribute', null, {});
-
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.bulkInsert(
+			'attribute',
+			[
+				{
+					attribute_id: 1,
+					name: 'Size'
+				},
+				{
+					attribute_id: 2,
+					name: 'Color'
+				}
+			],
+			{}
+		);
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.bulkDelete('attribute', null, {});
+	}
 };
