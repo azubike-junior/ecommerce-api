cac40e3b5b55d772ac6c28c899a86c3f32a2d0b4
zubi <azu@gmail.com>
2020-08-11 11:55:51 +0100

    changed baseUrl

--- server/utils/variable.js
+++ server/utils/variable.js
@@ -2,4 +2,4 @@ export const MOVE_TO_CART = 'PRODUCT_MOVED_TO_CART';
 export const SAVE_PRODUCT = 'PRODUCT_SAVED';
 export const PAID = true;
 export const UNPAID = false;
-export const baseUrl = 'http://localhost:4040'
+export const baseUrl = process.env.baseUrl
